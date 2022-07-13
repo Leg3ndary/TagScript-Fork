638f8156068af1fb7043ce2aa771914fdcc36216
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-07-13 18:33:22 -0400

    Push

--- bTagScript/block/discordblocks/__init__.py
+++ bTagScript/block/discordblocks/__init__.py
@@ -5,3 +5,15 @@ from .embedblock import EmbedBlock
 from .reactblock import ReactBlock
 from .redirectblock import RedirectBlock
 from .requireblacklistblock import BlacklistBlock, RequireBlock
+
+__all__ = (
+    "CommandBlock",
+    "OverrideBlock",
+    "CooldownBlock",
+    "DeleteBlock",
+    "EmbedBlock",
+    "ReactBlock",
+    "RedirectBlock",
+    "BlacklistBlock",
+    "RequireBlock",
+)
